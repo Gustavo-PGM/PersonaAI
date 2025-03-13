a3d1127b721b355d826d9e313b914fdbbf7a2dc5
Gustavo-PGM <gustavocgomes06@gmail.com>
2025-03-13 10:05:39 -0300

    feat: Modelo de tela adicionado, junto com a estrutura da API do PersonAI

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask
-
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-
 from views import *
 
+load_dotenv()
+
+
+flask_env = os.getenv('FLASK_ENV', 'production')
+
+debug_mode = flask_env == 'development'
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=debug_mode)
--- views.py
+++ views.py
@@ -1,6 +1,26 @@
 from main import app
+from flask import render_template
+from google import generativeai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+PersonaAI = os.getenv('CHAT_API_KEY')
 
 
-@app.route("")
+def personai():
+    client = generativeai.Client(api_key="CHAT_API_KEY")
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="Explain how AI works",
+    )
+
+    return response
+
+    #print(response.text)
+
+
+@app.route("/")
 def home():
-    return "Teste"
+    return render_template("home.html")
